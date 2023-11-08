ed11ecb2219e73f47d80b663f8ff1eb9f05c4b84
Mortol <94522772+Mortol2003@users.noreply.github.com>
2023-11-08 05:15:52 +0300

    first commit

---
+++ app/src/main/java/com/topic2/android/notes/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.topic2.android.notes
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.compose.setContent
+import com.topic2.android.notes.theme.JetNotesTheme
+import com.topic2.android.notes.viewmodel.MainViewModel
+import com.topic2.android.notes.viewmodel.MainViewModelFactory
+import kotlinx.coroutines.DelicateCoroutinesApi
+import com.topic2.android.notes.util.screens.NotesScreen
+
+/**
+ * Main activity приложения.
+ */
+class MainActivity : AppCompatActivity() {
+
+  private val viewModel: MainViewModel by viewModels(factoryProducer = {
+    MainViewModelFactory(
+      this,
+      (application as NotesApplication).dependencyInjector.repository
+    )
+  })
+
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    setContent {
+      JetNotesTheme {
+        NotesScreen(viewModel = viewModel)
+
+      }
+    }
+  }
+}
